b0fbf8e14d9d4e2ab865ce41bf77716718498a6d
ApurvChatur <chaturapurv@gmail.com>
2025-02-18 21:52:51 +0530

    fix: backend url updated

--- site/frontend/src/love/iSocket/socket.jsx
+++ site/frontend/src/love/iSocket/socket.jsx
@@ -7,7 +7,7 @@ const getSocket = () => useContext(SocketContext)
 
 const SocketProvider = ({ children }) => {
   const socket = useMemo(() => {
-    return io(import.meta.env.VITE_ENVIRONMENT === "Production" ? "https://soup-tadpole-backend.onrender.com" : "http://localhost:8000", { withCredentials: true })
+    return io(import.meta.env.VITE_ENVIRONMENT === "Production" ? "https://backend-for-tadpole-application.onrender.com" : "http://localhost:8000", { withCredentials: true })
   }, []);
 
   return (
